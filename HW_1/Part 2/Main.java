3c0ed38081c2feadcde48d9a8151a702a7cb1ae9
KonstantinosPaschopoulos <sdi1500127@di.uoa.gr>
2020-03-31 19:03:06 +0300

    fin

--- HW_1/Part 2/Main.java
+++ HW_1/Part 2/Main.java
@@ -3,7 +3,6 @@ import java.io.*;
 
 class Main {
     public static void main(String[] argv) throws Exception {
-        System.out.println("Please type your program:");
         Parser p = new Parser(new Scanner(new InputStreamReader(System.in)));
         p.parse();
     }
