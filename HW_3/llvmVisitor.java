eefebc966995c254b9013fbd4a7d91056b95b108
KonstantinosPaschopoulos <sdi1500127@di.uoa.gr>
2020-05-07 18:18:02 +0300

    file, dir, emit

--- HW_3/llvmVisitor.java
+++ HW_3/llvmVisitor.java
@@ -1,3 +1,7 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import syntaxtree.*;
 import visitor.GJDepthFirst;
 
@@ -5,10 +9,44 @@ public class llvmVisitor extends GJDepthFirst<String, String> {
 
     mySymbolTable symbolTable;
     String fileName;
+    String dirName = "LLVM_output";
 
-    public llvmVisitor(mySymbolTable symbolTable, String fileName) {
+    public llvmVisitor(mySymbolTable symbolTable, String fileName) throws IOException {
         this.symbolTable = symbolTable;
-        this.fileName = fileName;
+        String fileName_only = new File(fileName).getName().replaceFirst("[.][^.]+$", "");
+        this.fileName = dirName + "/" + fileName_only + ".ll";
+
+        // Create a folder to hold the .ll files
+        File directory = new File(dirName);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+
+        // Make sure that the .ll file is new and empty
+        File myF = new File(this.fileName);
+        if (myF.exists()) {
+            myF.delete();
+            myF.createNewFile();
+        } else {
+            myF.createNewFile();
+        }
+
+        emit("yeet" + fileName + "\n");
+        emit("yeet" + fileName + "\n");
+        emit("yeet" + fileName + "\n");
+    }
+
+    private void emit(String buffer) {
+        File file = new File(this.fileName);
+        try {
+            FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(buffer);
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 
     /**
@@ -18,6 +56,11 @@ public class llvmVisitor extends GJDepthFirst<String, String> {
     */
     public String visit(Goal n, String argu) throws Exception {
         String _ret = null;
+
+        // Add vtable on top of file
+
+        // Add boilerplate
+
         n.f0.accept(this, argu);
         n.f1.accept(this, argu);
         n.f2.accept(this, argu);
